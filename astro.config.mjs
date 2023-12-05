94b9021923148c1a128f6ff723b2ec650a019920
Gary Preston <gary@mups.co.uk>
2023-12-05 23:29:20 +0000

    Upgrade astro to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   integrations: [vue({ appEntrypoint: "/src/pages/_app" })],
 });
