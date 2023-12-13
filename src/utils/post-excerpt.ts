826d54e00089a977bb53ed34fa826c2c3ff3b2e5
Gary <gary@mups.co.uk>
2023-12-13 22:16:18 +0000

    Word limit optional if "more" marker used.

--- src/utils/post-excerpt.ts
+++ src/utils/post-excerpt.ts
@@ -7,17 +7,23 @@ export interface ExcerptOptions {
   words?: number;
   maxLength?: number;
   marker?: string;
+  markerLimit?: boolean;
 }
 
 const DefaultOptions: ExcerptOptions = {
   words: 60,
+  maxLength: 0,
+  marker: "more",
+  markerLimit: true,
 };
 
 export async function createExcerpt(md: string, options: ExcerptOptions = {}): Promise<string> {
+  const opt = {...DefaultOptions, ...options}
+
   let text = (
     await remark()
       .use(remarkExcerpt, {
-        identifier: options.marker ?? DefaultOptions.marker,
+        identifier: opt.marker
       })
       .use(stripMarkdown, {
         remove: ["heading"],
@@ -27,11 +33,14 @@ export async function createExcerpt(md: string, options: ExcerptOptions = {}): P
     .toString()
     .trim();
 
-  const numWords = options.words ?? DefaultOptions.words ?? 0;
-  const maxLength = options.maxLength ?? DefaultOptions.maxLength ?? 0;
+  // When marker present, do not apply limits.
+  if (opt.markerLimit || !md.includes(`<!--${opt.marker}-->`)) {
+    if (opt.words && opt.words > 0) text = text.split(" ").slice(0, opt.words).join(" ");
+    if (opt.maxLength && opt.maxLength > 0) text = text.slice(0, opt.maxLength);
+  }
 
-  if (numWords > 0) text = text.split(" ").slice(0, numWords).join(" ");
-  if (maxLength > 0) text = text.slice(0, maxLength);
-
-  return text;
+  if (['.', ','].includes(text.slice(-1)))
+    return text.slice(0, -1) + '...'
+  
+  return text + '...';
 }
