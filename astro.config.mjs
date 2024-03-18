049a4ddca4e2e16e083d5456cf363e9b923dbf8f
Gary <gary@mups.co.uk>
2024-03-18 19:25:43 +0000

    Add vercel hybrid mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue({ appEntrypoint: "/src/pages/_app" })],
+  integrations: [vue({
+    appEntrypoint: "/src/pages/_app"
+  })],
+  output: "hybrid",
+  adapter: vercel()
 });
