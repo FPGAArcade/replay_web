4303fb47eb63b3aeab6d541828e42b8000aeb03d
Gary Preston <gary@mups.co.uk>
2025-01-16 00:18:51 +0000

    Default setting now covers hybrid mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue({
     appEntrypoint: "/src/pages/_app"
   })],
-  output: "hybrid",
   adapter: vercel({
-    functionPerRoute: false,
   })
 });
